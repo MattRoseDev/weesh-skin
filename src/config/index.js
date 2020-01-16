30263b3477df9d185e527f54f1a7107f5c21f843
Mahdi <mahdineer@pm.me>
2020-01-16 20:41:51 +0330

    feat: implement config directory

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const PORT = 4000
+const API_URL = `http://localhost:${PORT}/api/v1/graphql`
+const WS_URL = `ws://localhost:${PORT}/api/v1/graphql`
+
+export default {
+    PORT,
+    API_URL,
+    WS_URL
+}
