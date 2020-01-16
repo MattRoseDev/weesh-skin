6e66cfa0cf99e69d1ab7ffca18993158986b2e5b
Mahdi <mahdineer@pm.me>
2020-01-16 20:40:59 +0330

    feat: add username to AuthContext

--- src/contexts/auth/index.js
+++ src/contexts/auth/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { authReducer } from 'Root/reducers/auth'
+import helpers from 'Root/helpers'
 
+const { storage } = helpers
 export const AuthContext = React.createContext()
 
 const initialAuth = {
-    token: null
+    token: storage.get({ key: 'token' }) || false,
+    username: storage.get({ key: 'username' }) || false,
 }
 
 const AuthProvider = (props) => {
     const [auth, dispatch] = React.useReducer(authReducer, initialAuth)
+    React.useEffect(() => {
+        storage.set({ key: 'token', value: auth.token })
+        storage.set({ key: 'username', value: auth.username })
+    },[auth])
     return <AuthContext.Provider value={{auth, dispatch}}>
         {props.children}
     </AuthContext.Provider>
