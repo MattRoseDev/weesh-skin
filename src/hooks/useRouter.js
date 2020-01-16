3e5e02face00c3f010bd8ee7d17b383535efe5cc
Matt Rose <mattroseinbox@gmail.com>
2020-01-16 20:35:04 +0330

    feat: add hooks

---
+++ src/hooks/useRouter.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { __RouterContext } from 'react-router-dom'
+
+const useRouter = () => {
+    return React.useContext(__RouterContext)
+}
+
+export default useRouter
