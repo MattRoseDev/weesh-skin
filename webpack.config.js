08fd0164adaca67a99fa7a53a20f90533a1c2edb
Mahdi <mahdineer@pm.me>
2020-01-24 14:10:31 +0330

    perf: add compression plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 const path = require('path')
 const babelConfig = require('./babel.config.json')
 
@@ -63,7 +64,10 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: path.join(__dirname, './src', 'index.html'),
-            filename: './index.html'
-        })
-    ]
+            filename: 'index.html',
+            hash: true
+        }),
+        new CompressionPlugin()
+    ],
+    performance: { hints: false }
 }
