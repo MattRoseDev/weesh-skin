cbe299955866c9004782783d7f4b7219ed6b5421
Matt Rose <mattroseinbox@gmail.com>
2020-02-21 18:34:59 +0330

    perf: users api

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,22 +1,96 @@
 import gql from 'graphql-tag'
 
-const getUserByUsername = gql`
-    query getUserByUsername($username: String!){
-        getUserByUsername(username: $username) {
+const getUserByUsernameForUser = gql`
+    query getUserByUsernameForUser($username: String!){
+        getUserByUsernameForUser(username: $username) {
             id
             username
             firstName
             lastName
             bio
+            private
             unknown {
                 avatar
                 fullname
+            }
+            connection {
+                follower {
+                    id
+                    username
+                }
+                following {
+                    id
+                    username
+                }
+                status
+            }
+            followers {
+                paginate {
+                    totalDocs
+                }
+            }
+            following {
+                paginate {
+                    totalDocs
+                }
+            }
+            weesh {
+                weeshes {
+                    id
+                    link
+                    user {
+                        id
+                        username
+                        firstName
+                        lastName
+                        unknown {
+                            fullname
+                            avatar
+                        }
+                    }
+                    content 
+                    status
+                    like {
+                        weeshLikes {
+                            user {
+                                id
+                                username
+                            }
+                        }
+                        paginate {
+                            totalDocs
+                        }
+                    }
+                    isLiked {
+                        user {
+                            id
+                            username
+                        }
+                    }
+                    comment {
+                        weeshComments {
+                            user {
+                                id
+                                username
+                            }
+                            content
+                            updatedAt
+                        }
+                        paginate {
+                            totalDocs
+                        }
+                    }
+                    updatedAt
+                }
+                paginate {
+                    totalDocs
+                }
             }
         }
     }
 `
 
 export default {
-    getUserByUsername
+    getUserByUsernameForUser
 }
 
