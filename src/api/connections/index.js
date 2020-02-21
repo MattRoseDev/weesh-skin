911670e6bfe5896712acd7438e7cc0f529ff6125
Mahdi <mahdineer@pm.me>
2020-02-21 18:31:10 +0330

    feat: add connections api

---
+++ src/api/connections/index.js
@@ -0,0 +1,75 @@
+import gql from 'graphql-tag'
+
+const follow = gql`
+    mutation followUserConnectionForUser($userId: ID!){
+        followUserConnectionForUser(userId: $userId) {
+            follower {
+                id
+            }
+            following {
+                id
+            }
+            status
+        }
+    }
+`
+
+const unfollow = gql`
+    mutation unfollowUserConnectionForUser($userId: ID!){
+        unfollowUserConnectionForUser(userId: $userId) {
+            follower {
+                id
+            }
+            following {
+                id
+            }
+            status
+        }
+    }
+`
+
+const getFollowers = gql`
+    query getFollowersUserConnectionByUsernameForUser($username: String!){
+        getFollowersUserConnectionByUsernameForUser(username: $username) {
+            userConnections {
+                follower {
+                    id
+                    username
+                    firstName
+                    lastName
+                    unknown {
+                        avatar
+                        fullname
+                    }
+                }
+            }
+        }
+    }
+`
+
+const getFollowing = gql`
+    query getFollowingUserConnectionByUsernameForUser($username: String!){
+        getFollowingUserConnectionByUsernameForUser(username: $username) {
+            userConnections {
+                following {
+                    id
+                    username
+                    firstName
+                    lastName
+                    unknown {
+                        avatar
+                        fullname
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default {
+    follow,
+    unfollow,
+    getFollowers,
+    getFollowing,
+}
+
