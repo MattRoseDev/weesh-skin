8e49330d41dad880e98e3b9ad646f66cadc7b30b
Mahdi <mahdineer@pm.me>
2020-03-17 23:30:07 +0330

    feat: add explore context

---
+++ src/reducers/explore/index.js
@@ -0,0 +1,11 @@
+export const exploreReducer = (state, action) => {
+    switch(action.type) {
+        case 'EXPLORE': 
+            return {
+                ...state,
+                ...action.data
+            }
+        default:
+            return state
+    }
+}
