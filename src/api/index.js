21083fd86202acaa6fbadd93646b818e461cf6de
Mahdi <mahdineer@pm.me>
2020-03-17 23:29:07 +0330

    feat: add user context

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,15 @@
 import auth from './auth'
 import users from './users'
+import explore from './explore'
+import connections from './connections'
+import weeshes from './weeshes'
+import weeshComments from './weeshComments'
 
 export default {
     auth,
     users,
+    explore,
+    connections,
+    weeshes,
+    weeshComments,
 }
---
+++ src/contexts/user/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { userReducer } from 'Root/reducers/user'
+
+export const UserContext = React.createContext()
+
+const initialUser = null
+
+const UserProvider = (props) => {
+    const [user, dispatch] = React.useReducer(userReducer, initialUser)
+   
+    return <UserContext.Provider value={{ user, dispatch }}>
+        {props.children}
+    </UserContext.Provider>
+}
+
+export default UserProvider
