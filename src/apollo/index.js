1652067e7caf7910a4cfb8b72ee115b8af101b87
Mahdi <mahdineer@pm.me>
2020-03-28 19:47:30 +0430

    feat: config apollo links

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,7 +1,11 @@
 import { ApolloClient } from 'apollo-boost'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { createUploadLink } from 'apollo-upload-client'
 import { setContext } from 'apollo-link-context'
+import { ApolloLink, split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import config from 'Root/config'
 import helpers from 'Root/helpers'
 
@@ -9,12 +13,25 @@ const { storage } = helpers
 
 const cache = new InMemoryCache()
 
-const httpLink = new HttpLink({
+const uploadLink = new createUploadLink({
     uri: `${config.API_URL}`,
 })
 
+const wsLink = new WebSocketLink({
+    uri: `${config.WS_URL}`,
+    options: {
+        reconnect: true
+    }
+})
+
+const httpLink = new HttpLink({
+    uri: `${config.API_URL}`,
+    headers: {
+        token: storage.get({ key: 'token' })
+    }
+})
+
 const authLink = setContext((_, { headers }) => {
-    
     const token = storage.get({ key: 'token' })
 
     return {
@@ -25,9 +42,19 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
+const terminatingLink = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query)
+        return (
+            kind === 'OperationDefinition' && operation === 'subscription'
+        )
+    },
+    wsLink,
+)
+
 const client = new ApolloClient({
+    link: ApolloLink.from([terminatingLink, authLink, uploadLink]),
     cache,
-    link: authLink.concat(httpLink),
 })
 
 export default client
