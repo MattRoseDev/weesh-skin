f10875bf99b5dfe51c3ca3c028c2d362408bca53
Matt Rose <mattroseinbox@gmail.com>
2020-03-28 19:44:37 +0430

    feat: implement weeshLikes api

---
+++ src/api/weeshLikes/index.js
@@ -0,0 +1,107 @@
+import gql from 'graphql-tag'
+
+const like = gql`
+    mutation likeWeeshForUser($weeshId: ID!){
+        likeWeeshForUser(weeshId: $weeshId) {
+            id
+            user {
+                id
+                username
+                firstName
+                lastName
+                avatarAddress
+            }
+            weesh {
+                id
+                link
+                user {
+                    id
+                    username
+                    firstName
+                    lastName
+                    avatarAddress
+                    unknown {
+                        fullname
+                        avatar
+                    }
+                }
+                content 
+                status
+                like {
+                    weeshLikes {
+                        user {
+                            id
+                            username
+                        }
+                    }
+                    paginate {
+                        totalDocs
+                    }
+                }
+                isLiked {
+                    user {
+                        id
+                        username
+                    }
+                }
+            }
+            updatedAt
+        }
+    }
+`
+
+const dislike = gql`
+    mutation dislikeWeeshForUser($weeshId: ID!){
+        dislikeWeeshForUser(weeshId: $weeshId) {
+            id
+            user {
+                id
+                username
+                firstName
+                lastName
+                avatarAddress
+            }
+            weesh {
+                id
+                link
+                user {
+                    id
+                    username
+                    firstName
+                    lastName
+                    avatarAddress
+                    unknown {
+                        fullname
+                        avatar
+                    }
+                }
+                content 
+                status
+                like {
+                    weeshLikes {
+                        user {
+                            id
+                            username
+                        }
+                    }
+                    paginate {
+                        totalDocs
+                    }
+                }
+                isLiked {
+                    user {
+                        id
+                        username
+                    }
+                }
+            }
+            updatedAt
+        }
+    }
+`
+
+export default {
+    like,
+    dislike,
+}
+
