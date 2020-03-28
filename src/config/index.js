6f5b571ac4bc7c64d5318a5bff53def27af78d4b
Matt Rose <mattroseinbox@gmail.com>
2020-03-28 19:51:43 +0430

    feat: implement local and server config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,32 @@
-const PORT = 4000
-const API_URL = `http://localhost:${PORT}/api/v1/graphql`
-const WS_URL = `ws://localhost:${PORT}/api/v1/graphql`
+const PORT = 5000
+let API_URL, WS_URL, DOMAIN_URL,PROTOCOL, UPLOAD_URL
+
+const SOURCE = 'liara'
+const DEBUG = true
+
+switch (SOURCE) {
+    case 'liara':
+        PROTOCOL = 'https'
+        DOMAIN_URL = 'weesh-core.liara.run'
+        UPLOAD_URL = `${PROTOCOL}://${DOMAIN_URL}/public`
+        API_URL = `${PROTOCOL}://${DOMAIN_URL}/api/v1/graphql`
+        WS_URL = `ws://${DOMAIN_URL}/graphql`
+        break;
+    case 'localhost':
+        PROTOCOL = 'http'
+        DOMAIN_URL = `localhost:${PORT}`
+        UPLOAD_URL = `${PROTOCOL}://${DOMAIN_URL}/public`
+        API_URL = `${PROTOCOL}://${DOMAIN_URL}/api/v1/graphql`
+        WS_URL = `ws://${DOMAIN_URL}/graphql`
+        break;
+}
 
 export default {
     PORT,
+    PROTOCOL,
+    UPLOAD_URL,
     API_URL,
-    WS_URL
+    WS_URL,
+    DOMAIN_URL,
+    DEBUG
 }
