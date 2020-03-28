2af17d9f0179b9791474d16e949c42262244340c
Matt Rose <mattroseinbox@gmail.com>
2020-03-28 19:45:06 +0430

    feat: implement weeshComments api

--- src/api/weeshComments/index.js
+++ src/api/weeshComments/index.js
@@ -3,14 +3,38 @@ import gql from 'graphql-tag'
 const add = gql`
     mutation addWeeshCommentForUser($weeshId: ID!, $content: String!, $parentId: ID){
         addWeeshCommentForUser(weeshId: $weeshId, content: $content, parentId: $parentId) {
-            id
             user {
                 id
                 username
                 firstName
                 lastName
+                avatarAddress
+                unknown {
+                    fullname
+                    avatar
+                }
             }
-            content 
+            content
+            children {
+                weeshComments {
+                    user {
+                        id
+                        username
+                        firstName
+                        lastName
+                        avatarAddress
+                        unknown {
+                            fullname
+                            avatar
+                        }
+                    }
+                    content
+                    updatedAt
+                }
+                paginate {
+                    totalDocs
+                }
+            }
             updatedAt
         }
     }
