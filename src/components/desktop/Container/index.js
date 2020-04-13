118f0de192cf1940f8b494257514bba9b6581963
Matt Rose <mattroseinbox@gmail.com>
2020-04-14 03:58:49 +0430

    feat: implement container components for desktop

---
+++ src/components/desktop/Container/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const StyledContainer = styled.div`
+    ${({ margin }) => margin && css`
+        margin: ${margin};
+    `};
+    ${({width}) => width ? css`
+        min-width: ${width};
+        max-width: ${width};
+        width: ${width};
+    ` : css`    
+        min-width: 60rem;
+        max-width: 60rem;
+        width: 60rem;
+    `};
+`
+
+export default (props) => {
+    return <StyledContainer {...props}>
+        {props.children}
+    </StyledContainer>
+}
