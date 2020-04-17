fd1454cb8c5e731eeeee65f7a9474360abdc945c
Mahdi <mahdineer@pm.me>
2020-04-17 10:28:55 +0430

    feat: implement add comment and reply for weesh page context

--- src/reducers/weeshPage/index.js
+++ src/reducers/weeshPage/index.js
@@ -1,19 +1,54 @@
 export const weeshPageReducer = (state, action) => {
+    let weeshComments
     switch(action.type) {
         case 'SET_WEESH': 
             return {
                 ...state,
                 ...action.data
             }
+        case 'SET_TEXTAREA': 
+            return {
+                ...state,
+                ...action.data
+            }
         case 'ADD_COMMENT': 
-            let weeshComments = state.comment.weeshComments
-            weeshComments.push(action.data)
+            if(state.reply) {
+                console.log('reply')
+                weeshComments = state.comment.weeshComments
+                weeshComments = weeshComments.map(weeshComment => {
+                    if (weeshComment.id == state.reply.parentId) {
+                        weeshComment.children.weeshComments.push(action.data)
+                    }
+                    return weeshComment
+                })
+            } else {
+                console.log('comment')
+                weeshComments = state.comment.weeshComments
+                weeshComments.push(action.data)
+            }
+            
             return {
                 ...state,
                 comment: {
                     ...state.comment,
                     weeshComments
-                }
+                },
+                reply: null
+            }
+        case 'SET_REPLY': 
+            let reply = {
+                parentId: action.data.id,
+                id: action.data.user.id,
+                username: action.data.user.username,
+            }
+            return {
+                ...state,
+                reply
+            }
+        case 'UNSET_REPLY': 
+            return {
+                ...state,
+                reply: null
             }
         default:
             return state
