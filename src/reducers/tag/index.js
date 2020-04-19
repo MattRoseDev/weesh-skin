72ffd3a5b47ea81ef8ddbaeac4155848d402756f
Matt Rose <mattroseinbox@gmail.com>
2020-04-19 11:06:08 +0430

    feat: add tag contexts

---
+++ src/reducers/tag/index.js
@@ -0,0 +1,11 @@
+export const tagReducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_TAG_DATA': 
+            return {
+                ...state,
+                ...action.data
+            }
+        default:
+            return state
+    }
+}
