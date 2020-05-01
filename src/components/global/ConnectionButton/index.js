9f1f19bc05036c0c30fed5606940e02f6a81a2dc
Mahdi <mahdineer@pm.me>
2020-05-01 14:43:22 +0430

    feat: implement connection button for global

---
+++ src/components/global/ConnectionButton/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ConnectionButton from './ConnectionButton'
+
+export default (props) => {
+    const handleStatus = () => {
+        if (props.connection) {
+            const { status } = props.connection
+            const { user } = props
+
+            switch (status) {
+                case 0: return <ConnectionButton user={user} type='FOLLOW' />
+                case 1: return <ConnectionButton user={user} type='REQUEST' />
+                case 2: return <ConnectionButton user={user} type='FOLLOWING' />
+            }
+        }
+    }
+    return handleStatus()
+}
