1d7475115ccc322654798092512be14dcc474756
Matt Rose <mattroseinbox@gmail.com>
2020-05-04 15:20:09 +0430

    feat: hide scrollbar for all elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Container from 'Root/container'
+import 'Root/css/style.css'
 
 render(
     <Container />,
-    document.querySelector('#root')
+    document.querySelector('#app')
 )
