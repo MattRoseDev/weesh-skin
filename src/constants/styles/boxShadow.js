483c6c96ca5d2ff79f128fffad1a9eb597bea170
Mahdi <mahdineer@pm.me>
2020-05-04 15:26:14 +0430

    feat: add boxShadow styles to constants

--- src/constants/styles/boxShadow.js
+++ src/constants/styles/boxShadow.js
@@ -2,12 +2,18 @@ import { css } from 'styled-components'
 import themes from 'Root/constants/themes'
 
 const primary = {
+    bold: css`
+        box-shadow: 0 0 3px 1px ${({theme}) => theme.colors.boldShadow};
+    `,
     normal: css`
-        box-shadow: 1px 1px 3px 1px ${themes.light.colors.light};
+        box-shadow: 0 0 2px 1px ${({theme}) => theme.colors.shadow};
+    `,
+    straight: css`
+        box-shadow: 0 0 2px 1px ${({theme}) => theme.colors.shadow};
     `,
     small: css`
-        box-shadow: 0 1px 3px rgba(50,50,93,.075), 0 1px 0 rgba(0,0,0,.01);
-    `
+        box-shadow: 0 1px 3px ${({ theme }) => theme.colors.light};
+    `,
 }
 
 export default {
