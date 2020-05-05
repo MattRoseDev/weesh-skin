db5d8f4679b79e556e2f56f03a66317a70f1d4be
Mahdi <mahdineer@pm.me>
2020-05-05 19:28:21 +0430

    feat: implement toggle component for global

---
+++ src/components/global/Toggle/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+import C from 'Root/constants'
+
+const StyledContainer = styled.label`
+    position: relative;
+    display: inline-block;
+    width: 3rem;
+    height: 1.75rem;
+`
+
+const StyledSwitch = styled.span`
+    border: 1px solid ${({ theme }) => theme.colors.light};
+    background-color: ${({theme}) => theme.colors.background};
+    border-radius: 50rem;
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    transition: all .25s ease;
+    &:before{
+        position: absolute;
+        left: .25rem;
+        bottom: .25rem;
+        border-radius: 50%;
+        content: "";
+        width: 1.125rem;
+        height: 1.125rem;
+        background-color: ${({theme}) => theme.colors.background};
+        box-shadow: 0 0 2px 2px ${({ theme }) => theme.colors.light};
+        transition: all .25s ease;
+    }
+`
+
+const StyledInput = styled.input`
+    display: none;
+    
+    &:checked + ${StyledSwitch} {
+        border: 1px solid ${({theme}) => theme.colors.foreground};
+        background-color: ${({theme}) => theme.colors.foreground};
+        &:before {
+            box-shadow: unset;
+            transform: translateX(20px);
+        }
+    }
+`
+
+export default (props) => {
+    return <StyledContainer>
+        <StyledInput 
+            type='checkbox' 
+            defaultChecked={props.checked || undefined} 
+            onChange={props.onChange}
+            onInput={props.onInput}
+        />
+        <StyledSwitch/>
+    </StyledContainer>
+}
