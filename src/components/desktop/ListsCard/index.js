72fd45fc360f9d3adc64dceda053d772dd2c71dc
Matt Rose <mattroseinbox@gmail.com>
2020-05-13 11:53:34 +0430

    feat: implement list card component for desktop

---
+++ src/components/desktop/ListsCard/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import C from 'Root/constants'
+import uuid from 'uuid'
+
+const StyledContainer = styled.div`
+    ${C.styles.boxShadow.primary.normal};
+    border-radius: .75rem;
+`
+
+const StyledTitle = styled.div`
+    color: ${({theme}) => theme.colors.foreground};
+    font-weight: bolder;
+    font-size: 1.25rem;
+    padding: 1rem;
+`
+
+const StyledItem = styled.div`
+    padding: .5rem;
+    border-top: 1px dashed ${({ theme }) => theme.colors.light};
+`
+
+export default (props) => {
+    return <StyledContainer {...props}>
+        {props.title && <StyledTitle>
+            {props.title}
+        </StyledTitle>}
+        {props.items && props.items.map(item => <StyledItem key={uuid()}>
+            {item}
+        </StyledItem>)}
+    </StyledContainer>
+}
