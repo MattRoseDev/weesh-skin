4b5860daf63613770e5db4fe0818a23cb4611737
Matt Rose <mattroseinbox@gmail.com>
2020-05-16 00:31:34 +0430

    feat: implement notification page

---
+++ src/contexts/notifications/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { notificationsReducer } from 'Root/reducers/notifications'
+
+
+export const NotificationsContext = React.createContext()
+
+const initialNotifications = {
+    store: {},
+    isEmpty: false
+}
+
+const NotificationsProvider = (props) => {
+    const [notifications, dispatch] = React.useReducer(notificationsReducer, initialNotifications)
+
+    return <NotificationsContext.Provider value={{ notifications, dispatch }}>
+        {props.children}
+    </NotificationsContext.Provider>
+}
+
+export default NotificationsProvider
