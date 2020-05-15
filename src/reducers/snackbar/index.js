47a6c2a385ec91b65655d9e7547e11df0e26d768
Mahdi <mahdineer@pm.me>
2020-05-15 15:22:24 +0430

    feat: implement snackbar component for desktop and mobile

---
+++ src/reducers/snackbar/index.js
@@ -0,0 +1,21 @@
+export const snackbarReducer = (state, action) => {
+    switch(action.type) {
+        case 'SET_DATA': 
+            return {
+                ...state,
+                ...action.data
+            }
+        case 'SHOW': 
+            return {
+                ...state,
+                visible: true
+            }
+        case 'HIDE': 
+            return {
+                ...state,
+                visible: false
+            }
+        default:
+            return state
+    }
+}
