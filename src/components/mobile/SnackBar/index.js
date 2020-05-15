47ae2cd0bbf9f8d7150883ce956840da8ae4271c
Matt Rose <mattroseinbox@gmail.com>
2020-05-15 15:22:24 +0430

    feat: implement snackbar component for desktop and mobile

---
+++ src/components/mobile/SnackBar/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { SnackBarContext } from 'Root/contexts/snackbar'
+import Icon from 'Root/components/global/Icon'
+import styled from 'styled-components'
+import C from 'Root/constants'
+
+const StyledContainer = styled.div`
+    ${C.styles.flex.flexRow};
+    ${C.styles.flex.justifyContentEnd};
+    ${C.styles.flex.alignItemsCenter};
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 20;
+    padding: .75rem;
+`
+
+const StyledContent = styled.div`
+    ${C.styles.flex.flexRow};
+    ${C.styles.flex.alignItemsCenter};
+    width: 100%;
+    border-radius: .5rem;
+    padding: 1rem;
+    background: ${({ theme, snackbar }) => theme.colors[snackbar.background] || 'unset'};
+    color: ${({ theme, snackbar }) => theme.colors[snackbar.color] || 'unset'};
+`
+
+const StyledMessage = styled.div`
+    color: inherit;
+    font-size: .85rem;
+`
+
+const StyledIcon = styled.div`
+    ${C.styles.flex.flexRow};
+    ${C.styles.flex.center};
+    color: inherit;
+    padding: 0 .5rem 0 0;
+`
+
+export default (props) => {
+    const { snackbar } = React.useContext(SnackBarContext)
+    return snackbar.visible ? <StyledContainer>
+        <StyledContent snackbar={snackbar} {...props}>
+            <StyledIcon>
+                <Icon icon={snackbar.icon} color={snackbar.color} />
+            </StyledIcon>
+            <StyledMessage>
+                {snackbar.message}
+            </StyledMessage>
+        </StyledContent>
+    </StyledContainer> : ''
+}
---
+++ src/contexts/snackbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { snackbarReducer } from 'Root/reducers/snackbar'
+
+export const SnackBarContext = React.createContext()
+
+const initialSnackBar = {
+    message: '',
+    icon: '',
+    background: 'foreground',
+    color: 'background',
+    visible: false
+}
+
+const SnackBarProvider = (props) => {
+    const [snackbar, dispatch] = React.useReducer(snackbarReducer, initialSnackBar)
+    
+    return <SnackBarContext.Provider value={{ snackbar, dispatch }}>
+        {props.children}
+    </SnackBarContext.Provider>
+}
+
+export default SnackBarProvider
