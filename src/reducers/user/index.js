7fdd08d8b1e0723f1dcae60c2a1e576a887e8ac6
Matt Rose <mattroseinbox@gmail.com>
2020-05-15 15:20:08 +0430

    perf: improve user reducer

--- src/reducers/user/index.js
+++ src/reducers/user/index.js
@@ -1,9 +1,20 @@
 export const userReducer = (state, action) => {
     switch(action.type) {
-        case 'SET_USER_DATA': 
+        case 'ADD_USER_DATA': 
             return {
                 ...state,
                 ...action.data
+            }
+        case 'REMOVE_WEESH': 
+            let weeshes = state.weesh.weeshes.filter(item => {
+                if(item.id != action.id) return item
+            })
+            return {
+                ...state,
+                weesh: {
+                    ...state.weesh,
+                    weeshes
+                }
             }
         default:
             return state
