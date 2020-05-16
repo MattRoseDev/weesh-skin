1cc5fac622ad664d30ad4db02e2d27d909e75234
Mahdi <mahdineer@pm.me>
2020-05-16 13:40:01 +0430

    feat: implement loading and loader component for global

---
+++ src/components/global/Loader/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import C from 'Root/constants'
+import Icon from 'Root/components/global/Icon'
+
+const StyledIcon = styled.div`
+    ${C.styles.spin};
+    ${C.styles.flex.inlineFlexRow};
+    color: ${({ theme, color }) => theme.colors[color]};
+    padding: 0;
+    margin: 0;
+`
+
+export default (props) => {
+    return <StyledIcon>
+        <Icon icon='Loader' {...props} />
+    </StyledIcon>
+}
