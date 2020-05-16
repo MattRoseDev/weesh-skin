cac5ae40ca1edadbb976e7cdcb8d843f9397c33d
Matt Rose <mattroseinbox@gmail.com>
2020-05-16 13:32:03 +0430

    feat: implement requests and connections page

---
+++ src/app/client/mobile/RequestsPage/Content/index.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import uuid from 'uuid'
+import List from 'Root/components/mobile/List'
+import Loader from 'Root/components/global/Loader'
+import SliderTab from 'Root/components/global/SliderTab'
+import C from 'Root/constants'
+import { useQuery } from '@apollo/react-hooks'
+import useHistory from 'Root/hooks/useHistory'
+import api from 'Root/api'
+import { AuthContext } from 'Root/contexts/auth'
+
+const StyledContainer = styled.div``
+
+const StyledLoaderContainer = styled.div`
+    ${C.styles.flex.flexRow};
+    ${C.styles.flex.justifyContentCenter};
+    padding: 1rem;
+`
+
+const switchStatus = (status) => {
+    switch (status) {
+        case 'requests': return {
+            index: 'follower',
+            api: 'getRequests',
+            data: 'getRequestsUsersConnectionByIdForUser',
+        }
+    }
+}
+
+export default (props) => {
+    let url = props.match.url.split('/')
+    const { auth } = React.useContext(AuthContext)
+    const [state, setState] = React.useState(null)
+    const history = useHistory()
+    const [status, setStatus] = React.useState(url[url.length - 1])
+    const { data, called, error, loading } = useQuery(api.connections[switchStatus(status).api],{
+        fetchPolicy: 'no-cache',
+    })
+
+    if (auth.id != undefined && auth.username !== props.match.params.username) {
+        history.push(`/${auth.username}`)
+    }
+
+    React.useEffect(() => {
+        if (error) {
+            console.log(error)
+        }
+
+        if (called && data) {
+            const result = data[switchStatus(status).data].userConnections
+            setState(result)
+        }
+    }, [data, error])
+
+    return <StyledContainer>
+        {loading ? <StyledLoaderContainer>
+            <Loader size={20} strokeWidth={1.25} color='gray' />
+        </StyledLoaderContainer> : state && <List request={true} index={switchStatus(status).index} users={state}/>}
+    </StyledContainer>
+}
---
+++ src/components/mobile/List/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import UserItem from './UserItem'
+import TagItem from 'Root/components/global/List/TagItem'
+import helpers from 'Root/helpers'
+import uuid from 'uuid'
+
+const StyledContainer = styled.div`
+    max-width: 100vw;
+`
+
+export default (props) => {
+    return <StyledContainer>
+        {props.users.map(user => (
+            user.title != undefined ? <TagItem key={uuid()} {...user} /> : 
+            <UserItem request={props.request || undefined}
+            index={props.index || undefined}
+            key={uuid()} {...user} /> 
+                
+        ))}
+    </StyledContainer>
+}
