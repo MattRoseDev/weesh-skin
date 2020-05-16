d3c28648cc031c4230832cdcbf97d365513ce2fe
Matt Rose <mattroseinbox@gmail.com>
2020-05-16 13:40:01 +0430

    feat: implement loading and loader component for global

---
+++ src/components/global/Loading/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import C from 'Root/constants'
+import Loader from 'Root/components/global/Loader'
+
+const StyledContaner = styled.div`
+    height: 100%;
+    width: 100%;
+    ${C.styles.flex.flexRowCenter};
+    ${C.styles.flex.justifyContentCenter};
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.dark};
+    ${({ padding }) => padding && css`
+        padding: ${padding};
+    `};
+`
+
+export default (props) => {
+    return <StyledContaner {...props} >
+        <Loader {...props} />
+    </StyledContaner>
+}
