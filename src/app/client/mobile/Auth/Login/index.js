b8aa184b0b3daa01290138b8e0644e1d119f2fb5
Matt Rose <mattroseinbox@gmail.com>
2020-07-04 12:21:35 +0430

    perf: disable users select

--- src/app/client/mobile/Auth/Login/index.js
+++ src/app/client/mobile/Auth/Login/index.js
@@ -33,7 +33,7 @@ const StyledForgotPasswordLink = styled(Link)`
 const StyledJoinLink = styled(Link)`
     ${C.styles.flex.inlineFlexRow};
     ${C.styles.flex.justifyContentCenter};
-    border: 1px solid ${({theme}) => theme.colors.primary};
+    /* border: 1px solid ${({theme}) => theme.colors.primary}; */
     color: ${({theme}) => theme.colors.primary};
     margin: 0;
     text-decoration: none;
