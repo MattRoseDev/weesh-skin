49873291d284ac78b604c9219c36b4dbf9b878b9
Matt Rose <mattroseinbox@gmail.com>
2020-10-20 23:09:32 +0330

    feat: implement new showcase for mobile

---
+++ src/app/client/mobile/Home/Showcase/Card/index.js
@@ -0,0 +1,69 @@
+import React from "react"
+import styled, { css } from "styled-components"
+import { ReactSVG } from "react-svg"
+import C from "Root/constants"
+
+const StyledCard = styled.div`
+    ${({ flexDirection }) => {
+        if (flexDirection && flexDirection == "column") {
+            return css`
+                ${C.styles.flex.flexColumn};
+            `
+        } else {
+            return css`
+                ${C.styles.flex.flexRow};
+            `
+        }
+    }};
+    ${C.styles.flex.alignItemsCenter};
+    margin: 2rem 0.25rem;
+`
+const StyledContent = styled.div`
+    margin: 0;
+`
+const StyledTitle = styled.h3`
+    text-align: center;
+    color: ${({ theme }) => theme.colors.foreground};
+    text-transform: uppercase;
+    font-size: 1.5rem;
+
+    ${({ flexDirection }) => {
+        if (flexDirection && flexDirection == "column") {
+            return css`
+                margin: 1.5rem 0 0.5rem;
+            `
+        } else {
+            return css`
+                margin: 0.5rem 0;
+            `
+        }
+    }};
+`
+const StyledDescription = styled.p`
+    color: ${({ theme }) => theme.colors.foreground};
+    font-size: 1.25rem;
+    line-height: 2rem;
+    text-align: center;
+`
+
+export default props => {
+    return (
+        <StyledCard flexDirection={props.flexDirection || undefined}>
+            <ReactSVG
+                src={props.icon}
+                beforeInjection={svg => {
+                    svg.setAttribute(
+                        "style",
+                        `width: ${props.width}px;height: ${props.width}px;fill:${props.color}`,
+                    )
+                }}
+            />
+            <StyledContent>
+                <StyledTitle flexDirection={props.flexDirection || undefined}>
+                    {props.title}
+                </StyledTitle>
+                <StyledDescription>{props.description}</StyledDescription>
+            </StyledContent>
+        </StyledCard>
+    )
+}
---
+++ src/app/client/mobile/Home/Showcase/Section/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+import C from "Root/constants"
+
+const StyledContainer = styled.section`
+    ${C.styles.flex.flexColumn};
+    ${C.styles.flex.alignItemsCenter};
+    width: ${({ width }) => width || "100%"};
+    margin: 1rem 0 0;
+`
+
+const StyledTitle = styled.span`
+    border-radius: 50rem;
+    background-color: ${({ theme }) => theme.colors.foreground};
+    color: ${({ theme }) => theme.colors.background};
+    padding: 0.5rem 1rem;
+    margin: 2rem;
+    font-weight: bold;
+`
+
+export default ({ children, title, width }) => {
+    return (
+        <StyledContainer width={width || undefined}>
+            <StyledTitle>{title}</StyledTitle>
+            {children}
+        </StyledContainer>
+    )
+}
